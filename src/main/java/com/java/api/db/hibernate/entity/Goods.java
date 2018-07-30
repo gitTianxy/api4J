9b228e09f98c764bb8218446d96ea2d820fdad39
gitTianxy <txy_learning@163.com>
2018-07-30 16:29:33 +0800

    add: hibernate CRUD

---
+++ src/main/java/com/java/api/db/hibernate/entity/Goods.java
@@ -0,0 +1,70 @@
+package com.java.api.db.hibernate.entity;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Goods {
+    private int id;
+    private String name;
+    private Double price;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "price")
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Goods goods = (Goods) o;
+
+        if (id != goods.id) return false;
+        if (name != null ? !name.equals(goods.name) : goods.name != null) return false;
+        if (price != null ? !price.equals(goods.price) : goods.price != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (price != null ? price.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Goods[id:%s, name:%s, price:%s]", id, name, price);
+    }
+}
---
+++ src/test/java/com/java/api/hibernate/GoodsTest.java
@@ -0,0 +1,74 @@
+package com.java.api.hibernate;
+
+import com.java.api.db.hibernate.entity.Goods;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.text.DecimalFormat;
+import java.util.List;
+
+public class GoodsTest {
+    private SessionFactory sessionFactory;
+    private Session session;
+    private Transaction transaction;
+
+    @Before
+    public void init() {
+        Configuration configuration = new Configuration().configure();//创建配置对象
+        sessionFactory = configuration.buildSessionFactory();//创建会话工厂
+        session = sessionFactory.openSession();//开启会话
+        transaction = session.beginTransaction();//开启事务
+
+    }
+
+    @After
+    public void destroy() {
+        transaction.commit();//事务提交
+        session.close();//关闭会话
+        sessionFactory.close();//关闭会话工厂
+    }
+
+    @Test
+    public void save() {
+        DecimalFormat df = new DecimalFormat("#.00");
+        for (int i=0; i<10; i++) {
+            Goods g = new Goods();
+            g.setId(i);
+            g.setName("goods_" + i);
+            g.setPrice(Double.valueOf(df.format(Math.random() * 100)));
+            session.saveOrUpdate(g);
+        }
+    }
+
+    @Test
+    public void update() {
+        Goods g = session.find(Goods.class, 1);
+        g.setName(g.getName() + "_NEW");
+        session.update(g);
+    }
+
+    @Test
+    public void find() {
+        Goods g = session.find(Goods.class, 1);
+        System.out.println(g);
+    }
+
+    @Test
+    public void list() {
+        List<Goods> list = session.createQuery("From Goods").list();
+        for (Goods g : list) {
+            System.out.println(g);
+        }
+    }
+
+    @Test
+    public void delete() {
+        Goods g = session.find(Goods.class, 1);
+        session.delete(g);
+    }
+}
